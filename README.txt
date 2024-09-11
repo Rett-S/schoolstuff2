3bcdbbce9ba9b2562670f37b944e2c2b8c861d4d
Rett <rettswyers@gmail.com>
2024-09-11 06:12:45 -0500

    user.c changes

--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
-First and foremost, if the executables are not there, use the "make" command to run the makefile to create executables for user and oss
-
+1. First and foremost, if the executables are not there, use the "make" command to run the makefile to create executables for user and oss
+2. Afterward, you can enter "./user 5" to run the user executable, which will loop 5 times
--- user.c
+++ user.c
@@ -1,17 +1,15 @@
+//Rett Swyers - CS 4760
+
 #include<unistd.h>
 #include<sys/types.h>
 #include<stdio.h>
 #include<stdlib.h>
 int main(int argc, char** argv) {
-		printf("Hello from Child.c, a new executable!\n");
-		printf("My process id is: %d\n",getpid());
-		printf(" I got %d arguments: \n", argc);
+                int i;
+                for (i =1; i < 6; i++) {
+                        printf("USER PID:%d PPID:%d Iteration:%d before sleeping \n", getpid(), getppid(), i);
+                }
+                sleep(1);
+                printf("USER PID:%d PPID %d Iteration:%d after sleeping \n", getpid(),getppid(), 5);
 
-		int i;
-		for (i =0; i < argc; i++)
-				printf("|%s| ", argv[i]);
-		printf("\nChild is now ending.\n");
-
-		sleep(3);
-		return EXIT_SUCCESS;
-}
+                return EXIT_SUCCESS;

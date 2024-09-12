e60b14361306e22955c2dde90c94954c1cde082d
Rett <rettswyers@gmail.com>
2024-09-11 19:56:00 -0500

    Finishing user.c

--- user.c
+++ user.c
@@ -4,12 +4,15 @@
 #include<sys/types.h>
 #include<stdio.h>
 #include<stdlib.h>
+
 int main(int argc, char** argv) {
                 int i;
-                for (i =1; i < 6; i++) {
+                int x;
+                x = atoi(argv[1]);
+                for (i = 1; i <= x; i++) {
                         printf("USER PID:%d PPID:%d Iteration:%d before sleeping \n", getpid(), getppid(), i);
                 }
                 sleep(1);
-                printf("USER PID:%d PPID %d Iteration:%d after sleeping \n", getpid(),getppid(), 5);
+                printf("USER PID:%d PPID %d Iteration:%d after sleeping \n", getpid(),getppid(), x);
 
                 return EXIT_SUCCESS;
